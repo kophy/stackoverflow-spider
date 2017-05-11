2857463d537b08cc7f01dfb13d60df4fc5c7f751
kophy <wenjietao179@gmail.com>
2017-05-11 16:19:46 -0700

    data extraction part

--- stack/stack/items.py
+++ stack/stack/items.py
@@ -11,4 +11,5 @@ import scrapy
 class StackItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field();
+    url = scrapy.Field();
---
+++ stack/stack/spiders/stack_spider.py
@@ -0,0 +1,17 @@
+import scrapy
+from stack.items import StackItem
+
+class StackSpider(scrapy.Spider):
+    name = "stack";
+    allowed_domains = ["stackoverflow.com"];
+    start_urls = [
+        "http://stackoverflow.com/questions?pagesize=50&sort=newest",
+    ];
+
+    def parse(self, response):
+        questions = scrapy.Selector(response).xpath("//div[@class='summary']/h3");
+        for question in questions:
+            item = StackItem();
+            item["title"] = question.xpath("'a[@class='question-hyperlink']/text()").extract()[0];
+            item["url"] = question.xpath("a[@class='question-hyperlink']/@href").extract()[0];
+            yield item;
