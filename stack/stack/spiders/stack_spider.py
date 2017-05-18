24928ca8ec6d6eab3649b1a51227414b8b6940ed
kophy <wenjietao179@gmail.com>
2017-05-18 16:12:49 -0700

    clean code

--- stack/stack/spiders/stack_spider.py
+++ stack/stack/spiders/stack_spider.py
@@ -9,11 +9,11 @@ class StackSpider(scrapy.Spider):
     ];
 
     def parse(self, response):
-        questions = scrapy.Selector(response).xpath("//div[@class='summary']/h3");
+        questions = scrapy.Selector(response).xpath("//div[@class=\"summary\"]/h3");
         for question in questions:
             item = StackItem();
             item["title"] = question.xpath(
-                'a[@class="question-hyperlink"]/text()').extract()[0]
+                "a[@class=\"question-hyperlink\"]/text()").extract()[0];
             item["url"] = question.xpath(
-                'a[@class="question-hyperlink"]/@href').extract()[0]
+                "a[@class=\"question-hyperlink\"]/@href").extract()[0];
             yield item;
